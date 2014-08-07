558eac1944fc44f2e0f788b0c15ad6bc90d1e4b2
Jan Segre <jan@segre.in>
2014-08-07 19:49:39 -0300

    Make it easier to publish gh-pages.

--- Makefile.mk
+++ Makefile.mk
@@ -13,3 +13,10 @@ todos:
 #
 #
 # vim: noet sts=0 sw=8 ts=8 filetype=make
+
+
+.PHONY:
+publish:
+	@cp roboime-tdp.pdf gh-pages/roboime-tdp-latest.pdf
+	@cp roboime-tdp-larc.pdf gh-pages/roboime-tdp-larc-latest.pdf
+	@cd gh-pages && git add -u && git commit -m 'Updating PDFs.'; git push
